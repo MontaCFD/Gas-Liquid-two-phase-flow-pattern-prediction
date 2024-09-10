909506fa3045d644e13efe91b33fe88a39a61901
Montadhar Guesmi <Guesmi.Montadhar@icloud.com>
2024-09-10 09:46:00 +0200

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,6 @@
 import numpy as np
 from numpy import pi, e
 # Constants for file paths
-POSTPROC_PATH  = "/path/to/data/"
-SENSIVITY_PATH = "/path/to/logs/"
-
 # Numeric constants
 PI  = pi
 e   = e
